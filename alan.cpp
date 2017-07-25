c9c9ede567bb4783ee6546e088de1eb1603f9208
Patrick Cramer <pocramer14@tamu.edu>
2017-07-25 00:22:20 -0700

    added alan.cpp

---
+++ alan.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <Eigen/Dense>
+
+using Eigen::MatrixXd;
+using namespace std;
+
+int main()
+{
+	string gui_token; //stores messages recieved from Chess GUI
+	MatrixXd board(8,8); // 8x8 matrix to store current position
+
+	cout.setf(ios::unitbuf); //send outputs straight to GUI
+
+	while(getline(cin, gui_token)){
+		if(gui_token == "uci"){
+			cout << "id name Alan" << endl;
+			cout << "id author Patrick Cramer" << endl;
+			// set options here
+			cout << "uciok" << endl;
+		}
+		else if(gui_token == "quit"){
+			cout << "quitting" << endl;
+			break;
+		}
+		else if(gui_token == "isready"){
+			cout << "readyok" << endl;
+		}
+		else if(gui_token == "ucinewgame"){
+			//engine logic begins here
+		}
+	}
+	return 0;
+}
